ea19a3530b7eb582cab88097ec3401c957ef8571
Belen JN <belenjarabanavas31@gmail.com>
2022-12-20 17:12:05 +0100

    HomeScreen's title styled

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Image, Text} from 'react-native';
 import {globalStyles} from '../theme/appTheme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
   return (
     <>
       <Image
         source={require('../assets/pokebola.png')}
         style={globalStyles.pokeballBG}
       />
-      <Text>Home</Text>
+      <Text
+        style={{...globalStyles.title, top: top + 20, ...globalStyles.margin}}>
+        Pokedex
+      </Text>
     </>
   );
 };
